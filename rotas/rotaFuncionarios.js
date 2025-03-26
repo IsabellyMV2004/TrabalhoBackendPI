7cf8e8fdac720b5c26da766bd9578ce1e1846fdd
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2025-03-26 19:22:18 -0300

    aaaa

--- rotas/rotaFuncionarios.js
+++ rotas/rotaFuncionarios.js
@@ -1,7 +1,7 @@
 //Associar os métodos da camada de controle de produto 
 //à requisições GET, POST, PUT, PATCH e DELETE HTTP
 
-/*import { Router } from "express"; //micro-aplicação HTTP
+import { Router } from "express"; //micro-aplicação HTTP
 import funcionarioCtrl from "../Controle/funcionarioCtrl.js";
 
 const catCtrl = new funcionarioCtrl();
@@ -15,27 +15,4 @@ rotafuncionario.get("/:codigo", catCtrl.consultar);
 rotafuncionario.get("/",catCtrl.consultar);
 
 export default rotafuncionario;
-*/
-
-
-import rotaFuncionario from './rotas/rotaFuncionarios.js';
-
-dotenv.config();
-
-const host = "0.0.0.0";
-const porta = 4000;
-
-const app = express(); 
-app.use(express.json());
-
-
-app.use(cors({
-                "origin":"*",
-                "Access-Control-Allow-Origin":'*'
-        }));
-
-app.use(express.static('./publico'));
-
-
-app.use("/cadastro_funcionarios",rotaFuncionario);
 
